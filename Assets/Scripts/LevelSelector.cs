0d3d6c5235c05da6016ec18a9b013a2b37e82954
akshar36 <112136517+akshar36@users.noreply.github.com>
2023-10-17 02:16:12 -0700

    Level Selector Bug Fix
    Text attribute got mouse clicks uncalibrated. changed to textmeshpro

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -3,27 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using TMPro;
-
+using UnityEngine.UI;
 public class LevelSelector : MonoBehaviour
 {
     public int level;
-    public TextMeshProUGUI levelText;
+    // public TextMeshPro levelText;
     public static int chosenLevel = 1;
 
     void Start()
     {
+
     }
 
     // Update is called once per frame
     public void OpenScene()
     {
-
         if (level.ToString() == "0")
         {
             SceneManager.LoadScene("Tutorial1");
         }
         else if (level.ToString() == "1")
         {
+            Debug.Log("xxx");
             SceneManager.LoadScene("SampleScene");
         }
         else if(level.ToString() == "2") {
