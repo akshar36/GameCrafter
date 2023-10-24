40149d9055f4b6562e2b632afd6a1320edb06da1
Vasundhara Singh <123125086+vasu1406@users.noreply.github.com>
2023-10-23 19:02:49 -0700

    Scene Connection for teleportation

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class Teleport : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class Teleport : MonoBehaviour
     private GameObject chaser;
     public static int teleportUsageCount = 0;
     public SendData sendDataScript;
+    public string tutorialSceneName = "Tutorial1";
 
    
     void Start()
@@ -21,12 +23,25 @@ public class Teleport : MonoBehaviour
     {
         if(collision.tag == "Player")
         {
-            player.transform.position = new Vector2(portal.transform.position.x, portal.transform.position.y);
-            teleportUsageCount++;
-            Debug.Log("Teleport usage incremented to: " + teleportUsageCount);
+            SceneManager.LoadScene(tutorialSceneName);
+            StartCoroutine(ReturnToOriginalSceneAfterDelay(10f));
+            
+            // player.transform.position = new Vector2(portal.transform.position.x, portal.transform.position.y);
+            // teleportUsageCount++;
+            // Debug.Log("Teleport usage incremented to: " + teleportUsageCount);
         } 
         else if(collision.tag == "Chaser"){
             chaser.transform.position = new Vector2(portal.transform.position.x, portal.transform.position.y);
         }
     }
+    private IEnumerator ReturnToOriginalSceneAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        // This will load the original scene based on the scene's build index.
+        // Here we're assuming the original scene is at build index 0.
+        // You might want to change this if your original scene has a different index.
+        SceneManager.LoadScene(0);
+    }
 }
+
