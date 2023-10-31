4d4eb13d009f51ecf3535f20086865f4971c4f9e
Vasundhara Singh <123125086+vasu1406@users.noreply.github.com>
2023-10-31 14:20:44 -0700

    Implementing fire floor logic

--- Assets/FireFloor.cs
+++ Assets/FireFloor.cs
@@ -19,16 +19,36 @@ public class FireFloor : MonoBehaviour
     }
     void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Chaser"){
-            GameObject spawnedObject = null;
-            Vector2 collisionPoint = collision.GetContact(0).point;
-            Debug.Log(collisionPoint);
-            Vector2 roundedVector = new Vector2(Mathf.Round(collisionPoint.x), Mathf.Round(collisionPoint.y));
-            if(temp != roundedVector){ 
-                spawnedObject = Instantiate(objectToSpawn, collisionPoint, Quaternion.identity);
-            temp = roundedVector;
+        // if(collision.gameObject.tag == "Chaser"){
+        //     GameObject spawnedObject = null;
+        //     Vector2 collisionPoint = collision.GetContact(0).point;
+        //     Debug.Log(collisionPoint);
+        //     Vector2 roundedVector = new Vector2(Mathf.Round(collisionPoint.x), Mathf.Round(collisionPoint.y));
+        //     if(temp != roundedVector){ 
+        //         spawnedObject = Instantiate(objectToSpawn, collisionPoint, Quaternion.identity);
+        //     temp = roundedVector;
+        //     }
+        //     StartCoroutine(RemoveTileAfterDelay(spawnedObject, 10f));
+        // }
+
+        if (collision.gameObject.CompareTag("Chaser"))
+        {
+            Vector2 collisionNormal = collision.GetContact(0).normal;
+
+            if (collisionNormal.y > 0)
+            {
+                GameObject spawnedObject = null;
+                Vector2 collisionPoint = collision.GetContact(0).point;
+                Vector2 roundedVector = new Vector2(Mathf.Round(collisionPoint.x), Mathf.Round(collisionPoint.y));
+
+                if (temp != roundedVector)
+                { 
+                    spawnedObject = Instantiate(objectToSpawn, collisionPoint, Quaternion.identity);
+                    temp = roundedVector;
+                }
+
+                StartCoroutine(RemoveTileAfterDelay(spawnedObject, 10f));
             }
-            StartCoroutine(RemoveTileAfterDelay(spawnedObject, 3f));
         }
     }
     private IEnumerator RemoveTileAfterDelay(GameObject tile, float delay)
