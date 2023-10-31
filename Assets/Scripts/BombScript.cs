e9ed3884e4b987ac4cd881c0fb43820aa4cff34b
akshar36 <112136517+akshar36@users.noreply.github.com>
2023-10-30 22:12:11 -0700

    Removing bombs after time n

--- Assets/Scripts/BombScript.cs
+++ Assets/Scripts/BombScript.cs
@@ -1,24 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BombScript : MonoBehaviour {
     private float speed = 11f;
     private Rigidbody2D rb;
     private Vector2 screenBounds;
+    private int startTime = 90;
 
-
-    // Use this for initialization
     void Start () {
         rb = this.GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(-speed, 0);
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
-    // Update is called once per frame
+
     void Update () {
         if(screenBounds.x *-3  > transform.position.x){
             if(this.gameObject != null)
                 Destroy(this.gameObject);
+        }
+    }
+
+    void removeBomb(){
+        float TimeLeft = 10;
+        if(startTime > 80)
+        {
+            if(TimeLeft > 0)
+            {
+                TimeLeft -= Time.deltaTime;
+            }
+            else
+            {
+                TimeLeft = 0;
+                int totalShieldsCollected = 0;
+                if(LevelSelector.chosenLevel == 1){
+                    SceneManager.LoadScene("Scene1");
+                    } 
+                else if(LevelSelector.chosenLevel == 2){
+                    SceneManager.LoadScene("Scene2");
+                }
+                else if(LevelSelector.chosenLevel == 3){
+                    SceneManager.LoadScene("Scene3");
+                }
+            }
         }
     }
 }
