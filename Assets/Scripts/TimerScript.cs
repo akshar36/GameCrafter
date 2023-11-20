9481277e16825a0f534a10fb87f58518f3e3e4e1
guptaaks7 <122641035+guptaaks7@users.noreply.github.com>
2023-11-19 20:54:03 -0800

    changing lava colour and removing continue button

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -20,7 +20,9 @@ public class TimerScript : MonoBehaviour
     {
         RestartTxt.gameObject.SetActive(false);
         PlayAgainTxt.gameObject.SetActive(false);
-        NextLevelTxt.gameObject.SetActive(false);
+        if(NextLevelTxt){
+            NextLevelTxt.gameObject.SetActive(false);
+        }
         Debug.Log("set time called on start");
         if (!AreWeReturningToTheScene){
             setTime();
@@ -80,7 +82,9 @@ public class TimerScript : MonoBehaviour
             platformCount, iceCount, EvaderSpace.totalShieldsCollected.ToString(), ChaserAI.timesStuck.ToString(), ""));
         GameText.gameObject.SetActive(true);
         PlayAgainTxt.gameObject.SetActive(true);
-        NextLevelTxt.gameObject.SetActive(true);
+        if(NextLevelTxt){
+             NextLevelTxt.gameObject.SetActive(true);
+        }
         TimerTxt.gameObject.SetActive(false);
         Time.timeScale = 0f;
         setTime();
