bc9feb1597cd4a09f139e5d531cb4c0471a89e2e
Vasundhara Singh <123125086+vasu1406@users.noreply.github.com>
2023-11-26 16:22:18 -0800

    Late Activation of ghost Chaser

--- Assets/Scripts/Evader.cs
+++ Assets/Scripts/Evader.cs
@@ -499,7 +499,7 @@ public class Evader : MonoBehaviour
     {
         chaserController.StartChasing();
         timerController.StartTime();
-        Invoke("activateGhost", 10.0f);
+        Invoke("activateGhost", 15.0f);
         Invoke("openWormhole", 4.0f);
     }
 
